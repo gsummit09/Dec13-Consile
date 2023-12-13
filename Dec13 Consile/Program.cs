66a3f42d241617142ae38e5b42f42b9eb7313808
sumit <sumit@gmail.com>
2023-12-13 12:24:04 +0530

    Add project files.

---
+++ Dec13 Consile/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dec13_Consile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whats up");//class.method
+            Console.ReadLine();
+        }
+    }// how to execute of result
+    // press ctrl +f5
+}
